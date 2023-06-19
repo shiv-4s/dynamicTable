d7f1ef81f9f65ce7ecbc9ce80801ddccbf91679c
shiv-4s <shivambarya049@gmail.com>
2023-06-19 15:51:23 +0530

    fourthftr

--- force-app/main/default/lwc/dynamicTable/dynamicTable.js
+++ force-app/main/default/lwc/dynamicTable/dynamicTable.js
@@ -1,6 +1,6 @@
 import { LightningElement, track, wire } from 'lwc';
 import fetchSObjectRecord from '@salesforce/apex/DynamicTableController.fetchSObjectRecord';
-// import fetchSObjectFieldName from '@salesforce/apex/DynamicTableController.fetchSObjectFieldName';
+import fetchSObjectFieldName from '@salesforce/apex/DynamicTableController.fetchSObjectFieldName';
 import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -8,16 +8,24 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class DynamicTable extends NavigationMixin(LightningElement) {
 
-  
-    
     fetchedData;
     errorMessage;
     fieldList;
     recordId;
-    fieldName;
     fullData= [];
     fieldName;
     nameRec;
+
+    @wire(fetchSObjectFieldName)
+    airedField({data, erorr}){
+        if(data){
+            this.fieldName = data;
+        }
+        else if(erorr){
+            console.log("+++++++error++++++++ "  + erorr.body.message);
+        }
+    }
+
     //wire funtion to fetch the record from apex method fetchSObjectRecord
     @wire(fetchSObjectRecord)
     wiredSObject({data, error}){ 
@@ -25,7 +33,6 @@ export default class DynamicTable extends NavigationMixin(LightningElement) {
             this.fetchedData = JSON.parse(JSON.stringify(data));
             this.fetchedData.forEach(element => {
                 var rec =[];
-                this.fieldName = Object.keys(element);
                 this.fieldName.forEach(list => {
                     if(!(Object.hasOwn(element,list))){
                         let obj = new Object();
@@ -35,12 +42,9 @@ export default class DynamicTable extends NavigationMixin(LightningElement) {
                     rec.push(element[list]);
                 })
                 this.fullData.push(rec);
-                console.log('full data ',this.fullData);
             });
-            
             console.log("++++++++fetchedData+++++++ " + JSON.stringify(this.fetchedData));
-            console.log("++++++++fetchedData+++++++ " + JSON.stringify(this.fieldName));
-
+            console.log('++++++++fulldata++++++++ ',this.fullData);
         }
         else if(error){
             console.log("+++++++error++++++++ "  + error.body.message);
@@ -55,10 +59,11 @@ export default class DynamicTable extends NavigationMixin(LightningElement) {
         }
     }
 
+
     //method to navigate on a particular record in table
     goToRecordPage(event){
-        this.recordId = event.target.dataset.Id;
-        console.log("+++++++ " +  event.target);
+        this.recordId = event.target.dataset.id;
+        console.log("+++++++ " +  this.recordId);
           // Generate a URL to a User record page
           this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
