b82c721c0a143624ba2991a0f3e9f96643505371
shiv-4s <shivambarya049@gmail.com>
2023-06-20 19:33:08 +0530

    lightningdatatableftr

--- force-app/main/default/lwc/dynamicTable/dynamicTable.js
+++ force-app/main/default/lwc/dynamicTable/dynamicTable.js
@@ -1,77 +1,98 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import fetchSObjectRecord from '@salesforce/apex/DynamicTableController.fetchSObjectRecord';
 import fetchSObjectFieldName from '@salesforce/apex/DynamicTableController.fetchSObjectFieldName';
-import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { updateRecord } from 'lightning/uiRecordApi';
 
 
 
-export default class DynamicTable extends NavigationMixin(LightningElement) {
+export default class DynamicTable extends LightningElement {
 
-    fetchedData;
     errorMessage;
-    fieldList;
     recordId;
-    fullData= [];
-    fieldName;
-    nameRec;
+    columns=[]
+    saveDraftValues = [];
+    objName;
+    fieldValues;
+    data;
 
     @wire(fetchSObjectFieldName)
-    airedField({data, erorr}){
+    wiredField({data, erorr}){
         if(data){
-            this.fieldName = data;
+            console.log("+++++++data+++++++++ " + JSON.stringify(data));
+            data.forEach(element => {
+                this.objName = element.Object_Type__c;
+                console.log("+++++objName+++++ " + this.objName);
+                this.fieldValues = element.Query__c;
+                console.log("+++++fieldValues+++++ " + this.fieldValues);
+                let fldName;
+                fldName = this.fieldValues.split(',');
+                fldName.forEach(element => {
+                    if(element === "Name"){
+                        let elementValue = {label: element, fieldName: 'recordNameUrl', type: 'url', typeAttributes: {label: {fieldName : 'Name'}, target: '_self'}};
+                        this.columns = [...this.columns, elementValue];
+                    }
+                    else{
+                        let elementValue1 = {label: element, fieldName: element, editable: true};
+                        this.columns = [...this.columns, elementValue1];
+                    }
+                });
+
+            });
         }
         else if(erorr){
             console.log("+++++++error++++++++ "  + erorr.body.message);
         }
     }
 
-    //wire funtion to fetch the record from apex method fetchSObjectRecord
-    @wire(fetchSObjectRecord)
-    wiredSObject({data, error}){ 
-        if(data){
-            this.fetchedData = JSON.parse(JSON.stringify(data));
-            this.fetchedData.forEach(element => {
-                var rec =[];
-                this.fieldName.forEach(list => {
-                    if(!(Object.hasOwn(element,list))){
-                        let obj = new Object();
-                        obj[list] = '';
-                        Object.assign(element,obj);
-                    }
-                    rec.push(element[list]);
-                })
-                this.fullData.push(rec);
-            });
-            console.log("++++++++fetchedData+++++++ " + JSON.stringify(this.fetchedData));
-            console.log('++++++++fulldata++++++++ ',this.fullData);
-        }
-        else if(error){
-            console.log("+++++++error++++++++ "  + error.body.message);
-            this.errorMessage = error.body.message;
-                const event = new ShowToastEvent({
-                    title: 'Unable To Load Record',
-                    message: this.errorMessage,
-                    variant: 'error',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
+    @wire(fetchSObjectRecord, {objectName: '$objName', fields: '$fieldValues'})
+    wiredRecord(result) {
+        if (result.data) {
+            let orignalData = [];
+            result.data.forEach((ele) => {
+                // console.log('name>>>', ele.Name);
+                let tempData = Object.assign({}, ele);
+                tempData.recordNameUrl = '/' + tempData.Id;
+                orignalData.push(tempData);
+            })
+            this.data = orignalData;
+        } else if (result.error) {
+            this.error = result.error;
         }
     }
 
+    handleSave(event){
+        this.saveDraftValues = event.detail.draftValues;
+        //console.log('________saveDraftValues__________', JSON.stringify(this.saveDraftValues));
+        const inputsItems = this.saveDraftValues.slice().map(draft => {
+            const fields = Object.assign({}, draft);
+            //console.log('__________fields____________', JSON.stringify(fields));
+            return { fields };
+        });
 
-    //method to navigate on a particular record in table
-    goToRecordPage(event){
-        this.recordId = event.target.dataset.id;
-        console.log("+++++++ " +  this.recordId);
-          // Generate a URL to a User record page
-          this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-              recordId: this.recordId,
-              actionName: 'view'
-            }
+        const promises = inputsItems.map(recordInput => updateRecord(recordInput));
+        //console.log('________________promises____________________', JSON.stringify(promises));
+        Promise.all(promises).then(res => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Records Updated Successfully!!',
+                    variant: 'success'
+                })
+            );
+            return refreshApex(this.mainresult);
+        }).catch(error => {
+            //console.log('______error________',JSON.stringify(error));
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: 'An Error Occured!!',
+                    variant: 'error'
+                })
+            );
+        }).finally(() => {
+            this.saveDraftValues = [];
         });
     }
-
 }
